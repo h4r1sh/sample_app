c6b4e67134cf469346bae055b82685a6da029681
h4r1sh <harishrengarajan@gmail.com>
2014-12-29 13:14:20 +0530

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,11 @@ describe "Static Pages" do
       visit '/static_pages/home'
       page.should have_content('Sample')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_title("Ruby on Rails Sample App | Home")
+    end
   end
   
   describe "Help Page" do
@@ -16,12 +21,22 @@ describe "Static Pages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_title("Ruby on Rails Sample App | Help")
+    end
   end
   
   describe "About Page" do
     it "should have the content 'About'" do
       visit '/static_pages/about'
       page.should have_content('About')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_title("Ruby on Rails Sample App | About")
     end
   end
   
