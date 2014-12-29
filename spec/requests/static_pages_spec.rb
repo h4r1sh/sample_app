0bb8dd3748f35592e29c9d461d79f922f15d527d
h4r1sh <harishrengarajan@gmail.com>
2014-12-29 17:16:12 +0530

    Change routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,48 +6,53 @@ describe "Static Pages" do
 
   describe "Home Page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Sample')
     end
     
     it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_title("Ruby on Rails Sample App | Home")
+      visit root_path
+      page.should have_title("Ruby on Rails Sample App")
+    end
+
+    it "should not have custom path title" do
+      visit root_path
+      page.should_not have_title("| Home")
     end
   end
   
   describe "Help Page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
     
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_title("Ruby on Rails Sample App | Help")
     end
   end
   
   describe "About Page" do
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About')
     end
     
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_title("Ruby on Rails Sample App | About")
     end
   end
   
   describe "Contact Page" do
     it "should have content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_content('Contact')
     end
     
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_title("Ruby on Rails Sample App | Contact")
     end
   end
