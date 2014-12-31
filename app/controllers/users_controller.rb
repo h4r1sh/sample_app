254e0953f04edd783db59f30d7e558515dea8cea
h4r1sh <harishrengarajan@gmail.com>
2014-12-31 12:17:39 +0530

    Add signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to sample app!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
   def new
+    @user = User.new
   end
 
   def show
